ae07f47132486e4c0a409dd9887c2e2bf07a46d0
Senio Caires <seniocaires@gmail.com>
2019-06-23 00:30:40 -0300

    Pacote de testes

---
+++ src/test/java/com/github/brutils/javabrutils/DataUtilTest.java
@@ -0,0 +1,44 @@
+package com.github.brutils.javabrutils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+/**
+ * Classe de teste unitário para testar a classe {@link DataUtil}.
+ */
+public class DataUtilTest {
+
+	/**
+	 * Logger da classe.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(DataUtilTest.class.getName());
+
+	/**
+	 * Construtor padrão.
+	 */
+	public DataUtilTest() {
+		// JUnit requer um construtor público.
+	}
+
+	/**
+	 * Testa o método {@link DataUtil#stringParaDate(String)}.
+	 */
+	@Test
+	public final void stringParaDate1() {
+		LOGGER.log(Level.FINE, "Testando stringParaDate1().");
+		// TODO: Testar {@link DataUtil#stringParaDate(String)}
+
+	}
+	
+	/**
+	 * Testa o método {@link DataUtil#stringParaDate(String, String)}.
+	 */
+	@Test
+	public final void stringParaDate2() {
+		LOGGER.log(Level.FINE, "Testando stringParaDate2().");
+		// TODO: Testar {@link DataUtil#stringParaDate(String, String)}
+
+	}
+}
